2c72b038e099aad965e6554f44b2937d083a984f
Andy Klimczak <andyklimczak@fastmail.com>
2017-04-18 20:08:16 -0400

    cleaner

--- ch7/caesar.exs
+++ ch7/caesar.exs
@@ -1,12 +1,10 @@
 defmodule MyList do
   def caesar(word, n), do: build(word, n)
 
-  defp build([head], n), do: [add_to_letter(head, n)]
+  defp build([head], n), do: [add(head, n)]
   defp build([head | tail], n) do
-    [add_to_letter(head, n)] ++ build(tail, n)
+    [add(head, n)] ++ build(tail, n)
   end
-
-  defp add_to_letter(letter, n), do: add(letter, n)
 
   defp add(letter, n) when letter + n > 122 do
     letter + n - 26
