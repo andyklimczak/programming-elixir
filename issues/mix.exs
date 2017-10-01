cc3898c5ebf2f401a65cd90edc1b80658409cb4b
Andy Klimczak <andyklimczak@fastmail.com>
2017-10-01 13:52:14 -0400

    issues finish

--- issues/mix.exs
+++ issues/mix.exs
@@ -5,7 +5,9 @@ defmodule Issues.Mixfile do
     [
       app: :issues,
       version: "0.1.0",
+      name: "Issues",
       elixir: "~> 1.5",
+      escript: escript_config,
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
@@ -14,7 +16,7 @@ defmodule Issues.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :httpoison]
+      extra_applications: [:logger, :httpoison, :jsx]
     ]
   end
 
@@ -22,9 +24,14 @@ defmodule Issues.Mixfile do
   defp deps do
     [
       { :httpoison, "~> 0.4" },
-      { :jsx, "~> 2.0" }
+      { :jsx, "~> 2.0" },
+      { :ex_doc, github: "elixir-lang/ex_doc" }
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
